719afd839cdc0294a13c1a920909cf15d46d2c8e
nicholas hays <nicholas.hays@hotmail.com>
2016-01-29 19:51:49 -0800

    updated source files

---
+++ src/ALU.c
@@ -0,0 +1,6 @@
+#include "ALU.h"  /* Include the header (not strictly necessary here) */
+
+int add(int x)    /* Function definition */
+{
+    return x + 5;
+}
---
+++ src/ALU.h
@@ -0,0 +1,6 @@
+#ifndef ALU_H_   /* Include guard */
+#define ALU_H_
+
+int add(int x);  /* An example function declaration */
+
+#endif // FOO_H_
