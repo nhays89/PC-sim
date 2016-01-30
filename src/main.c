4ef556309ad6f855d1eeced8d3176f3fbb90cf32
nicholas hays <nicholas.hays@hotmail.com>
2016-01-29 19:50:09 -0800

    updated main

--- src/main.c
+++ src/main.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include "ALU.h"  /* Include the header here, to obtain the function declaration */
 
-int main(int argc, char *argv[]) {
-
-  GtkWidget *window;
-
-  gtk_init(&argc, &argv);
-
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_widget_show(window);
-  
-  g_signal_connect(window, "destroy",
-      G_CALLBACK(gtk_main_quit), NULL);  
-
-  gtk_main();
-
-  return 0;
+int main(void)
+{
+    int y = add(3);  /* Use the function here */
+    printf("%d\n", y);
+    return 0;
 }
