1c79aaf89fbe38cbbf5f41aaefb0ce81aa4f4790
nicholas hays <nicholas.hays@hotmail.com>
2016-02-05 11:38:19 -0800

    batch file to compile simulator

---
+++ gcc_gtk.bat
@@ -0,0 +1,10 @@
+@echo off
+ 
+set gtk_ver=gtk+-3.0
+pkg-config %gtk_ver% --cflags --libs >tmp.txt
+set /p pkg-info= <tmp.txt
+del tmp.txt
+ 
+rem echo %pkg-info%
+gcc main.c alu.c -o main.exe -Wall %pkg-info%
+main.exe
